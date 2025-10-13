794a36a2e95a56eab0bb59f3ebd704ddb401f0fa
sameerbasha17 <sksameerbasha703@gmail.com>
2025-10-14 00:16:49 +0530

    Added Method 2

---
+++ Method_2/Program01.java
@@ -0,0 +1,34 @@
+/* PS : Check given number is positive or negative.
+ * Input : a integer.
+ * Output : A message that prints the nubmer is "positive" or "negative" or "zero".
+*/
+
+import java.util.Scanner;
+
+public class Program01 
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number = sc.nextInt();
+        if(number ==0)
+        {
+            System.out.println("The number is zero.");
+        }
+        else
+        {
+            String result = number>0?"positive" : "negative";
+            System.out.println("The number is "+result+".");
+        }
+    }
+    
+}
+
+/* Output : 
+ 
+Enter a number : 
+9
+The number is positive.
+
+ */
