53560c46778b6e0a82ea12fed82c0e1d576996b1
sameerbasha17 <sksameerbasha703@gmail.com>
2025-10-17 02:11:48 +0530

    Added Program 10 in method 02 on 17/10/2025

---
+++ Method_2/Program10.java
@@ -0,0 +1,62 @@
+/* PS : Find the Sum of the Digits of a Number in Java.
+Given an integer input number, the objective is t0 Find the Sum of the Digits of a Number in Java Language. */
+
+import java.util.Scanner;
+
+public class Program10 
+{
+    public static void main(String [] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        String number = sc.nextLine();
+
+        Program10 obj = new Program10();
+        System.out.println("The sum of digit in " + number + " is : "+ obj.getSum(number));
+    }   
+
+    int getSum(String number)
+    {
+        int sum = 0;
+        for(int i = 0; i < number.length(); i++)
+        {
+            char ch = number.charAt(i);
+            if(Character.isDigit(ch))                          // Explanation is provided at bottom after output (refer there)
+            {
+            sum += ch - '0';
+            }      
+        }
+
+        return sum;
+    }
+}
+
+/* Output :
+ ===========
+ Case - 1 :
+ -----------
+Enter a number :
+143
+The sum of digit in 143 is : 8
+
+Case - 2 :
+----------
+Enter a number :
+-143
+The sum of digit in -143 is : 8
+
+*/
+
+/* Explanation for getSum()
+
+☆ In this method i am using a string to take the input from the user. The user enters a number but we store it as a string.
+☆ we access each and every character in the String by using String_name.charAt(index).
+☆ And we should check each character whether it is a digit or not .
+☆ If digit :
+    we calculate the sum . i have taken sum = sum + ch - '0'. Or in other words sum = sum + number.charAt(i) - '0' => It returns the actual number not the ASCII value since we subtracted it by '0' (or 48 in ASCII).
+☆ If not digit : (ex : '-','a',' ')
+    skip
+☆ Return the sum
+
+It is not the exact algorithm, it's like a notes for understanding purpose.
+*/
