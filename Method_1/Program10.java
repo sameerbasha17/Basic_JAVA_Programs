f6c48fe3af97ee87c486225d1c449f325a8a856c
sameerbasha17 <sksameerbasha703@gmail.com>
2025-10-17 01:11:56 +0530

    Added Program 10 in method 01 on 17/10/2025

---
+++ Method_1/Program10.java
@@ -0,0 +1,53 @@
+/* PS : Find the Sum of the Digits of a Number in Java.
+Given an integer input number, the objective is t0 Find the Sum of the Digits of a Number in Java Language. */
+
+import java.util.Scanner;
+
+public class Program10 
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number = sc.nextInt();
+
+        sum_Of_Digits(number);     // Calling with parameter
+
+        sc.close();
+    }   
+
+    static void sum_Of_Digits(int number)
+    {
+        int sum = 0, temp = 0;
+        int num = number;      // taken, because we want to print number using format specifier at the end of the loop. At the end of the loop number becomes zero, so i have taken a duplicate variable.
+        while(number != 0)
+        {
+            temp = number % 10;    // finds the remainder with 10 (last digit of the given number).
+            sum +=temp;            // Adding the each digit in the given number.
+            number /= 10;          // Eleminating the last digit of the given number by dividing number with 10.
+        }
+
+        System.out.printf("The sum of digits in %d is : %d%n",num,sum);
+    }
+}
+
+/* Output :
+ ============
+ Case - 1 :
+ ----------
+Enter a number : 
+143
+The sum of digits in 143 is : 8
+
+Case - 2 :
+----------
+Enter a number : 
+0
+The sum of digits in 0 is : 0
+
+Case - 3 :
+----------
+Enter a number : 
+-79
+The sum of digits in -79 is : -16   => which is mathematically wrong, so i write this program in method 2 in more effectient way, so you can check out there.
+ */
